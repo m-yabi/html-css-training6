71e6c82a082d30b2226a62c9533920abfd1ce710
h_yabiku <h_yabiku@atoj.co.jp>
2022-12-15 12:25:10 +0900

    ~main__visual  jQuery更新

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ $(function () {
     $("#header__menu").toggleClass("panelactive");
   });
 });
+
+const swiper = new Swiper(".swiper", {
+  // 無限ループにするか
+  loop: true,
+  allowTouchMove: false,
+
+  // ふわっとなるスピード
+  speed: 1000,
+
+  // 自動再生
+  autoplay: {
+    // 次のスライドに切り替わる速さ
+    delay: 5000,
+  },
+
+  // フェードにしたい場合はこれを入れるだけ！
+  effect: "fade",
+});
